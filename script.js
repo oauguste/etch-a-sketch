366519031949e790c81d1d79f067d3e23f469076
Osnac <augustecopy@gmail.com>
2022-10-11 16:43:47 -0400

    mouseOver functionality added

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const mainGrid = document.querySelector('.outerBorder');
 
 
+// Dynamic boxes creation
+// user is going to select boxes x boxes
+// (Width of the div divided by user selection = 
+// height + width of boxes ( minus 2 for the border))
+// user boxes selection squared = i
+
+
 // Creating X amount of Divs
 for (let i = 0; i < 256; i++){
     const boxDiv = document.createElement('div');
     boxDiv.setAttribute('id','boxes');
     mainGrid.appendChild(boxDiv);
+    //boxDiv.classList.add('classN');
+    boxDiv.addEventListener('mouseover', function(){
+        boxDiv.classList.add("specialClass");
+      });
+      
+};
     
-}
