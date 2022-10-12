d483440f8b286855c7e5ad4d33eb6d8a4ac2c3d7
Osnac <augustecopy@gmail.com>
2022-10-11 20:48:06 -0400

    Program v1 done.

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
-const mainGrid = document.querySelector('.outerBorder');
 
 
-// Dynamic boxes creation
-// user is going to select boxes x boxes
-// (Width of the div divided by user selection = 
-// height + width of boxes ( minus 2 for the border))
-// user boxes selection squared = i
+
+let button = document.getElementById("#buttons");
+document.querySelector('#buttons').addEventListener('click', promptMe);
+
+  const mainGrid = document.querySelector(".outerBorder");
+  let howManyBoxes = 16;
+  let askUserWidth = 400 / howManyBoxes;
+  for (let i = 0; i < Math.pow(howManyBoxes, 2); i++) {
+  const boxDiv = document.createElement("div");
+  boxDiv.setAttribute("id", "boxes");
+  
+  boxDiv.style.width = askUserWidth - 2 + "px";
+  boxDiv.style.height = askUserWidth - 2 + "px";
+  mainGrid.appendChild(boxDiv);
+  //Hover effect
+  boxDiv.addEventListener("mouseover", function () {
+  boxDiv.classList.add("specialClass");
+  });
+  }
 
 
-// Creating X amount of Divs
-for (let i = 0; i < 256; i++){
-    const boxDiv = document.createElement('div');
-    boxDiv.setAttribute('id','boxes');
-    mainGrid.appendChild(boxDiv);
-    //boxDiv.classList.add('classN');
-    boxDiv.addEventListener('mouseover', function(){
-        boxDiv.classList.add("specialClass");
-      });
-      
+
+  function promptMe() {
+  const mainGrid = document.querySelector(".outerBorder");
+  // removes all children from node
+  while (mainGrid.hasChildNodes()) {
+    mainGrid.removeChild(mainGrid.firstChild);
+  }
+  // adds children to node
+  let howManyBoxes = prompt("how many boxes");
+  if (howManyBoxes > 100){
+    howManyBoxes = prompt("Enter a number below 100");
+  }
+  else if (howManyBoxes <101){
+  let askUserWidth = 400 / howManyBoxes;
+
+  for (let i = 0; i < Math.pow(howManyBoxes, 2); i++) {
+  const boxDiv = document.createElement("div");
+  boxDiv.setAttribute("id", "boxes");
+  
+  boxDiv.style.width = askUserWidth - 2 + "px";
+  boxDiv.style.height = askUserWidth - 2 + "px";
+  mainGrid.appendChild(boxDiv);
+  //Hover effect
+  boxDiv.addEventListener("mouseover", function () {
+  boxDiv.classList.add("specialClass");
+  });
 };
-    
+}
+else{
+    return;
+};
+  
+};
+
+function removeAllChildNodes(mainGrid) {
+    while (mainGrid.boxDiv) {
+        mainGrid.removeChild(parent.boxDiv);
+    }
+}
